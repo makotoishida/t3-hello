e775fc4f45b6cedf675abc8ba785297a049f1eeb
Makoto Ishida <makotoishida@gmail.com>
2023-02-26 16:33:17 -1000

    Initial set up.

---
+++ src/pages/_app.tsx
@@ -0,0 +1,30 @@
+import { MantineProvider } from "@mantine/core";
+import { type Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
+import { type AppType } from "next/app";
+
+import { api } from "../utils/api";
+
+import "../styles/globals.css";
+
+const MyApp: AppType<{ session: Session | null }> = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
+  return (
+    <SessionProvider session={session}>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          /** Put your mantine theme override here */
+          colorScheme: "light",
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+    </SessionProvider>
+  );
+};
+
+export default api.withTRPC(MyApp);
---
+++ src/pages/index.tsx
@@ -0,0 +1,49 @@
+import { type NextPage } from "next";
+import Head from "next/head";
+import { useState } from "react";
+
+import { Calendar, DatePicker } from "@mantine/dates";
+import { api } from "../utils/api";
+
+function Demo() {
+  const [value, setValue] = useState(new Date());
+
+  return (
+    <div className="p-4 flex gap-4  bg-white">
+      <>
+        <DatePicker />
+        <hr />
+        <br />
+        <Calendar
+          onClick={() => console.log("onClick")}
+          onChange={(value) => value && setValue(value)}
+          value={value}
+        />
+      </>
+    </div>
+  );
+}
+
+const Home: NextPage = () => {
+  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+
+  return (
+    <>
+      <Head>
+        <title>T3 Hello</title>
+        <meta name="description" content="Hello World create-t3-app" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+            Hello <span className="text-[hsl(280,100%,70%)]">T3</span>!
+          </h1>
+          <Demo />
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default Home;
