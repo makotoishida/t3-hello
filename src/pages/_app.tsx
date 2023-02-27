1d1c5df4d0daf7446e133b61990f4696fb505806
Makoto Ishida <makotoishida@gmail.com>
2023-02-26 22:26:25 -1000

    Add Mantine's AppShell etc.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,30 +1,68 @@
-import { MantineProvider } from "@mantine/core";
+"use client";
+import { Footer, MantineProvider, useMantineTheme } from "@mantine/core";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
 
+import { AppShell, Header, Navbar } from "@mantine/core";
+import { useState } from "react";
 import { api } from "../utils/api";
-import { emotionCache } from '../utils/emotion-cache';
-
-import "../styles/globals.css";
+import { emotionCache } from "../utils/emotion-cache";
+// import "../styles/globals.css";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
+  const theme = useMantineTheme();
+  const [opened, setOpened] = useState(false);
+
   return (
     <SessionProvider session={session}>
       <MantineProvider
         withGlobalStyles
         withNormalizeCSS
         theme={{
-        /** Put your mantine theme override here */
+          /** Put your mantine theme override here */
           colorScheme: "light",
           // colorScheme: "dark",
         }}
         emotionCache={emotionCache}
       >
-        <Component {...pageProps} />
+        <AppShell
+          padding="sm"
+          navbar={
+            <Navbar
+              p="xs"
+              width={{ xs: 200 }}
+              hiddenBreakpoint="xs"
+              hidden={!opened}
+            >
+              <div>Navbar</div>
+            </Navbar>
+          }
+          header={
+            <Header height={60} p="xs">
+              <div>Header</div>
+            </Header>
+          }
+          styles={(theme) => ({
+            main: {
+              backgroundColor:
+                theme.colorScheme === "dark"
+                  ? theme.colors.dark[8]
+                  : theme.colors.gray[0],
+            },
+          })}
+          footer={
+            <Footer height={60} p="md">
+              Application footer
+            </Footer>
+          }
+        >
+          {/* Your application here */}
+          <Component {...pageProps} />
+        </AppShell>
       </MantineProvider>
     </SessionProvider>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,19 @@
+"use client";
 import { type NextPage } from "next";
 import Head from "next/head";
 import { useState } from "react";
 
-import { Calendar, DatePicker } from "@mantine/dates";
+import { Container, Space, Text, Title } from "@mantine/core";
+import { DatePicker } from "@mantine/dates";
 import { api } from "../utils/api";
 
 function Demo() {
   const [selectedDate, setSelectedDate] = useState<Date | null>(new Date());
 
   return (
-    <div className="p-4 flex gap-4  bg-white">
-      <>
-        <DatePicker
-          onChange={setSelectedDate}
-          value={selectedDate}
-        />
-        <hr />
-        <br />
-        <Calendar
-          onClick={() => console.log("onClick")}
-          onChange={setSelectedDate}
-          value={selectedDate}
-          month={selectedDate || undefined}
-        />
-      </>
-    </div>
+    <Container p="4">
+      <DatePicker onChange={setSelectedDate} value={selectedDate} />
+    </Container>
   );
 }
 
@@ -35,17 +24,27 @@ const Home: NextPage = () => {
     <>
       <Head>
         <title>T3 Hello</title>
-        <meta name="description" content="Hello World create-t3-app" />
+        <meta name="description" content="Hello T3" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            Hello <span className="text-[hsl(280,100%,70%)]">T3</span>!
-          </h1>
-          <Demo />
-        </div>
-      </main>
+      <Container p="sm">
+        <Title
+          align="center"
+          order={2}
+          variant="gradient"
+          gradient={{ from: "indigo", to: "cyan", deg: 45 }}
+        >
+          ようこそ T3 App へ!
+        </Title>
+        <Space h={24}></Space>
+        <Demo></Demo>
+        {Array.from(new Array(20).keys()).map((i) => (
+          <>
+            <Space h={24}></Space>
+            <Text>{i + 1} これはテキストです。</Text>
+          </>
+        ))}
+      </Container>
     </>
   );
 };
